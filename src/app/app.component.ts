fe6b84f381040c810b1597b8dbec7ffacd9b09f0
Farid Ardiansyah <faridardiansyah061@gmail.com>
2024-08-23 16:51:22 +0700

    Files upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./budget-planner/login/login.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, CommonModule, MatIconModule, LoginComponent]
+})
+export class AppComponent {
+  title = 'budget-planner';
+}
